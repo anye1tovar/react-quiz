2ccfe77b720c35c28614eeb03eee1cf6efeda1b6
anye1tovar <natovar@uniboyaca.edu.co>
2023-10-13 16:56:07 -0500

    :sparkles: Answers logic

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,17 +2,25 @@ import { useContext } from "react";
 import Answer from "./Answer";
 import { QuizContext } from "../contexts/quiz";
 
-const Question = ({ questions }) => {
-  const [quizState] = useContext(QuizContext);
+const Question = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
   const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
   return (
     <>
       <div className="question">{currentQuestion.question}</div>
       <div className="answers">
-        <Answer />
-        <Answer />
-        <Answer />
-        <Answer />
+        {quizState.answers.map((answer, index) => (
+          <Answer
+            answerText={answer}
+            key={index}
+            index={index}
+            currentAnswer={quizState.currentAnswer}
+            correctAnswer={currentQuestion.correctAnswer}
+            onSelectAnswer={(answerText) =>
+              dispatch({ type: "SELECT_ANSWER", payload: answerText })
+            }
+          />
+        ))}
       </div>
     </>
   );
